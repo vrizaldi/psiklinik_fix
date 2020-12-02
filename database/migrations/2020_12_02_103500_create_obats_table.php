3a5c222f29c9b1e2fd1427c1530edae3dad5cc56
Verdy Noorghifari <vrizaldi@gmail.com>
2020-12-02 20:21:20 +0700

    First commit

---
+++ database/migrations/2020_12_02_103500_create_obats_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateObatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('obats', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_obat');
+            $table->float('dosis', 8, 2);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('obats');
+    }
+}
